bf986db3d1f5c9c99f2ff36a89c0044bb2c0c77d
raghav <phpngdeveloper@gmail.com>
2024-04-20 23:03:20 +0530

    updated code

--- config/index.js
+++ config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   secret: process.env.NODE_ENV === "production" ? "secret" : "secret",
   tokenExpiryTime: process.env.tokenExpiryTime || 60, //in minutes
   jwtTokenExpiryTime: process.env.jwtTokenExpiryTime || "60m", //in minutes,
-  mongoDBURI: "mongodb://127.0.0.1:27017/cms_logistics", //"mongodb+srv://kickmunna:kickmunna_123456@cluster0.vtbp6dz.mongodb.net/cms_logistics"  //"mongodb://127.0.0.1:27017/cms_logistics"
+  mongoDBURI: "mongodb+srv://kickmunna:kickmunna_123456@cluster0.vtbp6dz.mongodb.net/cms_logistics"  //"mongodb://127.0.0.1:27017/cms_logistics"
 };
